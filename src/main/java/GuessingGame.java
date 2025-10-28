a544213d2239315fcb2024383984caf18dd8d746
georgehaan08 <03george.haan@kamsc.org>
2025-10-27 21:27:57 -0400

    Implement guessing game logic and user prompts

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,11 +1,28 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessingGame {
     public static void main(String[] args) {
-        // TODO: Prompt the user with a welcome message
-        // TODO: Generate a random secret number between 1 and 100 (inclusive)
-        // TODO: Use a do...while loop to read guesses until correct
-        // TODO: Print "Too low" / "Too high" / "You got it!" accordingly
-        // TODO: Use Scanner for input
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        int actual = rand.nextInt(100);
+        int guess;
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("I'm thinking of a number between 1 and 100.");
+        do
+        {
+            System.out.println("Enter your guess! ");
+            guess = sc.nextInt();
+            if(guess < actual)
+            {
+                System.out.println("Too low! Try again.");
+            }
+            else if(guess > actual)
+            {
+                System.out.println("Too high! Try again.");
+            }
+        } while (guess != actual);
+        System.out.println("You got it!");
     }
 }
+
